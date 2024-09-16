32a8980bd77cb5d994e8189e46360350cdc134bd
batatinhachips <87585577+batatinhachips@users.noreply.github.com>
2024-09-15 23:16:57 -0300

    Delete alcantara directory

--- alcantara/controladora/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Limpa todas as variáveis de sessão
-session_destroy(); // Destrói a sessão
-header("Location: ../visao/index.php"); // Redireciona para a página de login
-exit();
-?>
